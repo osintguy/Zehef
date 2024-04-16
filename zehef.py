92c19870d390f3952706d84f8049b10d8ab18823
osintguy <106027918+osintguy@users.noreply.github.com>
2024-04-16 21:17:32 +0800

    Update zehef.py

--- zehef.py
+++ zehef.py
@@ -1,5 +1,8 @@
-if __name__ == '__main__':
+def custom_main():
     import sys; sys.dont_write_bytecode = True
     from lib.helpers import show_banner; show_banner()
     import asyncio
-    from main import main; asyncio.run(main())
+    from main import main; asyncio.run(main())    
+
+if __name__ == '__main__':
+    custom_main()
